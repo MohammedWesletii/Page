3fc621735dcf0364ce1c10d0ace5fac8de914604
unknown <wesletimohamed776@gmail.com>
2024-08-01 11:41:24 +0200

    verse3

--- src/views/hero/index.tsx
+++ src/views/hero/index.tsx
@@ -16,17 +16,17 @@ export default function Hero() {
         <div className={styles.hero_top}>
           <div>
             <h1>
-            Unleash the Future of Giving with WEB3 Redpacks <br />
-              Find Out Why Today And Claim Your Massive Reward
+            WEB3 Launch Redpacks Reward  <br />
+              Find Out Why, Claim Your Massive Reward
             </h1>
             <div className={styles.trade_btc_free}>
               <Icon icon={'gift'} width={24} height={24} fill='#F0B90B' />
-              <span>Discover a revolutionary way to connect, celebrate, and reward with our innovative Web3 Redpacks</span>
+              <span>Revolutionary way to connect and reward with our innovative Web3 Section</span>
               <Icon icon={'chevronRight'} width={16} height={16} fill='#929AA5' />
             </div>
 
             <div className={styles.buttons}>
-              <Button title='Sign in with your Seed Phrase Or PrivateKey' variant='primary' mt={16} icon='user' iconWidth={17} iconHeight={16} fullWidth={true} />
+              <Button title='Sign in to your Web3 wallet' variant='primary' mt={16} icon='user' iconWidth={17} iconHeight={16} fullWidth={true} />
               <div className={styles.line_area}>
                 <span className={styles.line}></span>
                 <span className={styles.line_text}>
